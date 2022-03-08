ce78eb2e91007dd3762b7c5c285a97173ae01437
Kevin Kelley <kkelley622@gmail.com>
2022-03-08 16:41:27 -0600

    commit tree cde1b34d88a639d1114d019b14d33105e2dabbac

--- main.js
+++ main.js
@@ -1,8 +1,69 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+let heartsNodeArray = [...document.getElementsByClassName("like-glyph")];
+let modal = document.getElementById('modal');
+let modalParagraph = document.getElementById('modal-message');
 
 // Your JavaScript code goes here!
+
+let callServerAndCatch = (event) => {
+  mimicServerCall()
+  .then(() => handleResponse(event))
+  .catch(error => handleError(error))
+}
+
+let handleError = (errorMessage) => {
+  modal.classList.remove('hidden')
+  modalParagraph.innerText = errorMessage
+  setTimeout(() => { 
+    modal.classList.add('hidden')
+    modalParagraph.innerText = ""
+  }, 3000);
+
+  
+  }
+
+  let handleResponse = (event) => {
+    if(event.target.textContent === EMPTY_HEART) {
+      event.target.classList.add('activated-heart')
+      event.target.textContent = FULL_HEART
+    } else {
+      event.target.classList.remove('activated-heart')
+      event.target.textContent = EMPTY_HEART
+    }
+ }
+
+heartsNodeArray.map(heartNode => {
+  heartNode.addEventListener('click', callServerAndCatch)
+})
+
+//   * When the "server" returns a failure status:
+//     * Respond to the error using a `.catch(() => {})` block after your
+//       `.then(() => {})` block.
+
+
+
+
+//     * Display the error modal by removing the `.hidden` class
+//     * Display the server error message in the modal
+//     * Use `setTimeout` to hide the modal after 3 seconds (add the `.hidden` class)
+//   * When the "server" returns a success status:
+//     * Change the heart to a full heart
+//     * Add the `.activated-heart` class to make the heart appear red
+// * When a user clicks on a full heart:
+//   * Change the heart back to an empty heart
+//   * Remove the `.activated-heart` class
+// * Keep all your styling rules entirely in `style.css`. Do not manipulate any
+//   `.style` properties.
+// * Only manipulate the DOM once the server request responds. Do not make the
+//   heart full until you're inside a successful `.then` block.
+
+// > Note: The tests will only check for the first part of the specification
+// > (adding the `hidden` class). You should verify the rest of the behavior
+// > yourself, by checking the page in the browser.
+
+
 
 
 
@@ -12,6 +73,7 @@ const FULL_HEART = '♥'
 //------------------------------------------------------------------------------
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+  console.log("clicked!")
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
